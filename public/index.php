05850b08617788745953f19a733c91b66c274ddb
Dmitriy Nesterovich <abcd-xyz@list.ru>
2022-09-08 19:41:16 +0700

    l9

--- public/index.php
+++ public/index.php
@@ -9,8 +9,17 @@ $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
-$app->post('/users', function ($request, $response) {
+/*$app->post('/users', function ($request, $response) {
     return $response->write('POST /users');
+});*/
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
 });
 
 $app->run();
