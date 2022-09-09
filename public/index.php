d5254989d7fb92c6392ab3315cac4b67b00698ff
Dmitriy Nesterovich <abcd-xyz@list.ru>
2022-09-09 20:33:03 +0700

    l10

--- public/index.php
+++ public/index.php
@@ -2,6 +2,27 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+
+/*use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
@@ -9,9 +30,9 @@ $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
-/*$app->post('/users', function ($request, $response) {
-    return $response->write('POST /users');
-});*/
+//$app->post('/users', function ($request, $response) {
+//    return $response->write('POST /users');
+//});
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
@@ -20,7 +41,7 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
 
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
-});
+});*/
 
 $app->run();
 
