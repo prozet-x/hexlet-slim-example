2271aa543bb1fad23d0c77da91ae2ccc5c4a7cbe
Dmitriy Nesterovich <abcd-xyz@list.ru>
2022-09-21 21:04:02 +0700

    l19

--- public/index.php
+++ public/index.php
@@ -91,26 +91,5 @@ function getUsers() {
     );
 }
 
-/*use Slim\Factory\AppFactory;
-
-$app = AppFactory::create();
-
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
-
-//$app->post('/users', function ($request, $response) {
-//    return $response->write('POST /users');
-//});
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    return $response->write("Course id: {$id}");
-});
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});*/
-
 $app->run();
 
